6f732a2ef3edede1f00766eab78d893ea9f95e43
unknown <cte0136@CP2082.tvb.bassilichi.it>
2015-12-30 15:00:28 +0100

    Add encrypting function

--- src/android/Crypto.java
+++ src/android/Crypto.java
@@ -4,19 +4,26 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
-import org.apache.cordova.PluginResult;
-import org.apache.cordova.PluginResult.Status;
 import org.json.JSONArray;
 import org.json.JSONException;
-
-import android.content.Context;
-import android.graphics.Rect;
-import android.util.DisplayMetrics;
-import android.view.View;
-import android.view.ViewTreeObserver.OnGlobalLayoutListener;
-import android.view.inputmethod.InputMethodManager;
+import org.json.JSONObject;
+import android.util.Base64;
+import java.security.Key;
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 public class Crypto extends CordovaPlugin {
+
+    public static final String TAG = "Crypto";
+
+    final protected static char[] hexArray = "0123456789abcdef".toCharArray();
+    private static final String AES_TRANSORMATION = "AES/CBC/PKCS7Padding";
+    private static final String AES = "AES";
+    private static final byte[] ZERO_KEY = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+    public Crypto() {
+    }
 
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
@@ -24,6 +31,26 @@ public class Crypto extends CordovaPlugin {
 
     public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
         if ("AESencrypt".equals(action)) {
+
+            JSONObject retObj = new JSONObject();
+            String phrase = args != null && args.length() > 0 ? args.getString(0) : "";
+            String chiave = args != null && args.length() > 1 ? args.getString(1) : "";
+            String encryptedValue = "";
+            byte[] decodedKey = Base64.decode(chiave, Base64.DEFAULT);
+
+            try{
+                Key skeySpec = new SecretKeySpec(decodedKey, AES);
+                IvParameterSpec ivParameterSpec = new IvParameterSpec(ZERO_KEY);
+                Cipher ecipher = Cipher.getInstance(AES_TRANSORMATION);
+                ecipher.init(Cipher.ENCRYPT_MODE, skeySpec, ivParameterSpec);
+                byte[] theByteArray = phrase.getBytes();
+                byte[] dstBuff = ecipher.doFinal(theByteArray);
+                encryptedValue = byteArrayToHexString(dstBuff);
+            }catch(Exception e){
+            }
+
+            retObj.put("encrypted", encryptedValue);
+            callbackContext.success(retObj);
 			
             return true;
         }
@@ -34,6 +61,15 @@ public class Crypto extends CordovaPlugin {
         return false;  // Returning false results in a "MethodNotFound" error.
     }
 
+    public static String byteArrayToHexString(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
 
 }
 
---
+++ www/crypto.js
@@ -0,0 +1,29 @@
+
+var argscheck = require('cordova/argscheck'),
+    channel = require('cordova/channel'),
+    utils = require('cordova/utils'),
+    exec = require('cordova/exec'),
+    cordova = require('cordova');
+
+channel.createSticky('onCordovaInfoReady');
+// Tell cordova channel to wait on the CordovaInfoReady event
+channel.waitForInitialization('onCordovaInfoReady');
+
+
+function Crypto() {
+
+    this.pluginName = "Crypto";
+
+	channel.onCordovaReady.subscribe(function() {
+
+    });
+}
+
+Crypto.prototype.encrypt = function(phrase, key, cb, opt) {
+	argscheck.checkArgs('fF', 'Crypto.encrypt', arguments);
+    exec(cb, function(err){cb(err);}, "Crypto", "AESencrypt", [phrase, key, opt]);
+};
+
+module.exports = new Crypto();
+
+
